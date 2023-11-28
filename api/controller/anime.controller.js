1a5813b7a9db82648345faca10e05f2ba3b3a7c3
Unedo Tampubolon <unedo.tampubolon@gmail.com>
2023-11-28 19:40:37 +0700

    refactor anime controller

--- api/controller/anime.controller.js
+++ api/controller/anime.controller.js
@@ -1,130 +1,106 @@
+const { BaseError } = require("../handler/error.handler")
+const paginationHendler = require("../handler/pagination.hendler")
+const { SuccesResponse } = require("../handler/succes.handler")
 const {
-  GetbyId,
-  GetEpisodesByAnimeId,
-  GetByName,
-  GetEpisodeId,
   SearchFromApi,
   SearchEpisodeApi,
   SearchEpisodesApi,
+  FindAnimeById,
+  FindAnimeByTitle,
+  FindEpisodesByAnimeId,
+  FindAnimeEpisodeById,
 } = require("../usecase/anime.usecase")
 
-const GetAnimeById = async (req, res) => {
-  const id = req.query.id
+const GetAnimeById = async (req, res, next) => {
+  const id = req.params.id
   try {
-    const data = await GetbyId(id)
+    const data = await FindAnimeById(id)
     if (!data) {
       SearchFromApi(id)
-      return res.status(404).json({
-        success: false,
-        messege: "Anime NotFound, Try Again Letter",
-      })
+      throw new BaseError("Anime Not Found, Try Again Letter", 404)
     }
-    return res.json({
-      success: true,
-      data,
-    })
+    const response = new SuccesResponse("Success Get Anime", data)
+    response.succes200(res)
   } catch (error) {
-    return res.status(500).json({
-      success: false,
-      messege: "Server Internal Error " + error,
-    })
+    next(new BaseError(error.message, error.statusCode))
   }
 }
-const GetByTitle = async (req, res) => {
+
+const GetAnimeByTitle = async (req, res, next) => {
   const title = req.params.title
+  const { page, perPage } = req.query
+  const { limit, offset } = paginationHendler(page, perPage)
   try {
-    const datas = await GetByName(title)
+    const datas = await FindAnimeByTitle(title, limit, offset)
     if (datas.length === 0) {
       SearchFromApi(title)
-      return res.status(404).json({
-        success: false,
-        messege: "Anime NotFound, Try Again Letter",
-      })
+      throw new BaseError("Anime Not Found, Try Again Letter", 404)
     }
-    return res.json({
-      success: true,
-      data: datas,
-    })
-  } catch (error) {
-    return res.status(500).json({
-      success: false,
-      messege: error.message,
-    })
-  }
-}
-const GetEpsByAnimeId = async (req, res) => {
-  const id = req.query.anime_id
-  try {
-    // check if anime is in table in not in table put it to table first
-    const checkAnime = await GetbyId(id)
-    if (!checkAnime) {
-      SearchFromApi(id)
-      return res.status(404).json({
-        success: false,
-        messege: "Anime NotFound Try Again Letter",
-      })
-    }
-    // now anime are in table or anime are not found
-    const datas = await GetEpisodesByAnimeId(id)
-    if (datas.length === 0) {
-      SearchEpisodesApi(id)
-      return res.status(404).json({
-        success: false,
-        messege: "Episode NotFound, Try Again Letter",
-      })
-    }
-    return res.json({
-      success: true,
+    const response = new SuccesResponse("Success Get Anime", {
+      page,
+      dataPage: datas.length,
       datas,
     })
+    response.succes200(res)
   } catch (error) {
-    return res.status(500).json({
-      success: false,
-      messege: error.message,
-    })
+    next(new BaseError(error.message, error.statusCode))
   }
 }
-const GetAnimeEpisodeById = async (req, res) => {
+
+const GetEpsByAnimeId = async (req, res, next) => {
+  const animeId = req.params.anime_id
+  try {
+    // check if anime is in table in not in table put it to table first
+    const checkAnime = await FindAnimeById(animeId)
+    if (!checkAnime) {
+      SearchFromApi(animeId)
+      throw new BaseError("Anime Not Found, Try Again Letter", 404)
+    }
+
+    // now anime are in table or anime are not found
+    const datas = await FindEpisodesByAnimeId(animeId)
+    if (datas.length === 0) {
+      SearchEpisodesApi(animeId)
+      throw new BaseError("Episodes Not Found, Try Again Letter", 404)
+    }
+    const response = new SuccesResponse("Success Get Episodes", datas)
+    response.succes200(res)
+  } catch (error) {
+    next(new BaseError(error.message, error.statusCode))
+  }
+}
+
+const GetAnimeEpisodeById = async (req, res, next) => {
   const id = req.params.id
   try {
     let animeId = id.split("-")
     animeId.pop()
     animeId.pop()
     animeId = animeId.join("-")
-    const anime = await GetbyId(animeId)
+    const anime = await FindAnimeById(animeId)
     if (!anime) {
       ;(async () => {
         await SearchFromApi(animeId)
         SearchEpisodeApi(id, animeId)
       })()
-      return res.status(404).json({
-        success: false,
-        messege: "Anime NotFound Try Again Letter",
-      })
+      throw new BaseError("Anime Not Found, Try Again Letter", 404)
     }
-    const datas = await GetEpisodeId(id)
+    const datas = await FindAnimeEpisodeById(id)
     if (datas.length === 0) {
-      SearchEpisodeApi(id,animeId)
-      return res.status(404).json({
-        success: false,
-        messege: "Episode NotFound, Try Again Letter",
-      })
+      SearchEpisodeApi(id, animeId)
+      throw new BaseError("Episodes Not Found, Try Again Letter", 404)
     }
-    return res.json({
-      success: true,
-      datas,
-    })
+    const response = new SuccesResponse("Success Get Episode", datas)
+    response.succes200(res)
   } catch (error) {
-    return res.status(500).json({
-      success: false,
-      messege: error.message,
-    })
+    next(new BaseError(error.message, error.statusCode))
   }
 }
 
+
 module.exports = {
   GetAnimeById,
-  GetByTitle,
+  GetAnimeByTitle,
   GetEpsByAnimeId,
   GetAnimeEpisodeById,
 }
